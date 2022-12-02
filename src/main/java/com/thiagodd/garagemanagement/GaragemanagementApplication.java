aefa1cd3b8f8341500d4bc34efdc70ef69ec3a99
Thiago Silva <silvaa.thiaod2@gmail.com>
2022-12-02 06:10:57 -0300

    Initial project setup

---
+++ src/main/java/com/thiagodd/garagemanagement/GaragemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.thiagodd.garagemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagodd/garagemanagement/GaragemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagodd.garagemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaragemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
